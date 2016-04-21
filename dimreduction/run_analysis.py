224abed7a08db53deaec7b772bceb2eb5c39184c
K. Champion <kpchamp@login2.hyak.local>
2016-04-20 20:37:18 -0700

    Changes to run_analysis.py to work on Hyak.

--- dimreduction/run_analysis.py
+++ dimreduction/run_analysis.py
@@ -6,14 +6,16 @@ import scipy.linalg as la
 from optimal_svht_coef import optimal_svht_coef
 import pickle
 
-fname="/Users/kpchamp/Dropbox (uwamath)/backup/research/python/data/20150727_detrend.h5"
+fname="/gscratch/riekesheabrown/kpchamp/data/m187201_150727_decitranspose_detrend.h5"
+#fname="/Users/kpchamp/Dropbox (uwamath)/backup/research/python/data/20150727_detrend.h5"
 f=tb.open_file(fname,'r')
 
 
 # note: total frames are 347973
 q, Tmax = f.root.data.shape
 Twin = Tmax
-n_samples = 347972
+#n_samples = 347972
+n_samples = 30000
 n_folds = 4
 if (n_samples % n_folds) != 0:
     raise ValueError("number of samples n_samples=%d is not a multiple of n_folds=%d",n_samples,n_folds)
