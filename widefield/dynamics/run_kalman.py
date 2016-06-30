b15c634d636fc5ef96b879a542f9c129656b3066
kpchamp <kpchamp@uw.edu>
2016-06-30 13:18:43 -0700

    wrote some code to test LDS fitting on actual data

---
+++ widefield/dynamics/run_kalman.py
@@ -0,0 +1,20 @@
+import pickle
+import numpy as np
+import tables as tb
+from widefield.dynamics.lds import lds_model
+
+mouseId = 'm187201'
+collectionDate = '150727'
+basepath = "/suppscr/riekesheabrown/kpchamp/data/"
+datapath = basepath + mouseId + "/" + collectionDate + "/data_detrend_mask.h5"
+f=tb.open_file(datapath,'r')
+X=f.root.data[:,:]
+
+# note: total frames are 347973
+Tmax, n_features = f.root.data.shape
+# actually use only first 347904 = 128*2718 frames (for full would be 366000 = 128*2859)
+Tmax = 347904
+Twin = Tmax/8
+
+lds = lds_model(X[0:Twin,:].T, 25)
+pickle.dump(lds, open('/suppscr/riekesheabrown/kpchamp/lds_model_test.pkl','w'))
