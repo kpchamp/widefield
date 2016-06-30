4e2413e0526919b0c993d1d3ca411ffe52e79a25
kpchamp <kpchamp@uw.edu>
2016-06-30 13:40:44 -0700

    updated run_kalman.py to only do one EM iteration for timing purposes

--- widefield/dynamics/run_kalman.py
+++ widefield/dynamics/run_kalman.py
@@ -9,12 +9,13 @@ basepath = "/suppscr/riekesheabrown/kpchamp/data/"
 datapath = basepath + mouseId + "/" + collectionDate + "/data_detrend_mask.h5"
 f=tb.open_file(datapath,'r')
 X=f.root.data[:,:]
+f.close()
 
 # note: total frames are 347973
-Tmax, n_features = f.root.data.shape
+Tmax, n_features = X.shape
 # actually use only first 347904 = 128*2718 frames (for full would be 366000 = 128*2859)
 Tmax = 347904
 Twin = Tmax/8
 
-lds = lds_model(X[0:Twin,:].T, 25)
+lds = lds_model(X[0:Twin,:].T, 1)    # do only one iteration of EM for timing purposes
 pickle.dump(lds, open('/suppscr/riekesheabrown/kpchamp/lds_model_test.pkl','w'))
