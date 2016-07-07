5d05b71dc76e079e09c8a247960b7ede228a6402
kpchamp <kpchamp@uw.edu>
2016-07-07 10:21:49 -0700

    added function for just getting moments

--- widefield/dimreduction/analyze_residuals.py
+++ widefield/dimreduction/analyze_residuals.py
@@ -29,3 +29,13 @@ def get_residual_and_moments(n_components,X):
         moments[i,3] = kurtosis(R[:,i], fisher=True, bias=False)
 
     return R,moments
+
+
+def get_moments(R):
+    moments = np.zeros((R.shape[1],4))
+    for i in range(R.shape[1]):
+        moments[i,0] = np.mean(R[:,i])
+        moments[i,1] = np.std(R[:,i])**2
+        moments[i,2] = skew(R[:,i], bias=False)
+        moments[i,3] = kurtosis(R[:,i], fisher=True, bias=False)
+    return moments
