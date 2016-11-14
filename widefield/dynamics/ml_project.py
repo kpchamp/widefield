d744872f442f417eae88db6ecb7195272d1115cc
kpchamp <kpchamp@uw.edu>
2016-11-14 08:41:04 -0800

    removed diagonalization of covariance matrices in SSM for testing; updated ml_project

--- widefield/dynamics/ml_project.py
+++ widefield/dynamics/ml_project.py
@@ -55,12 +55,13 @@ lr = pickle.load(open(basepath + "ml_project/lr.pkl",'r'))
 
 print "Fitting LGSSM"
 # Fit EM parameters for the model, based on the sampled data
-# model = LinearGaussianSSM(A=np.copy(lr.coefficients), C=np.eye(10))
-# model.fit_em(pca_data_train.T, max_iters=1000, exclude_list=['C'])
-# pickle.dump(model,open(basepath + "ml_project/lgssm.pkl",'w'))
-model = pickle.load(open(basepath + "ml_project/lgssm.pkl",'r'))
+model = LinearGaussianSSM(A=np.copy(lr.coefficients), C=np.eye(10))
+model.fit_em(pca_data_train.T, max_iters=1000, exclude_list=['C'])
+pickle.dump(model,open(basepath + "ml_project/lgssm.pkl",'w'))
+# model = pickle.load(open(basepath + "ml_project/lgssm.pkl",'r'))
 
-model_kf = KalmanFilter(transition_matrices=np.copy(lr.coefficients), observation_matrices=np.eye(10))
-model_kf.em(pca_data_train, em_vars={'transition_matrices', 'transition_covariance', 'observation_covariance',
-                    'initial_state_mean', 'initial_state_covariance'}, n_iter=np.where(model.LL==0)[0][0])
-pickle.dump(model_kf,open(basepath + "ml_project/kf.pkl",'w'))
+# model_kf = KalmanFilter(transition_matrices=np.copy(lr.coefficients), observation_matrices=np.eye(10))
+# model_kf.em(pca_data_train, em_vars={'transition_matrices', 'transition_covariance', 'observation_covariance',
+#                     'initial_state_mean', 'initial_state_covariance'}, n_iter=np.where(model.LL==0)[0][0])
+# pickle.dump(model_kf,open(basepath + "ml_project/kf.pkl",'w'))
+# model_kf = pickle.load(open(basepath + "ml_project/kf.pkl",'r'))
