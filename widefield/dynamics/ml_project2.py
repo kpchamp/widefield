4107005973ff883ab902dd23d401c2973666d2e9
kpchamp <kpchamp@uw.edu>
2016-11-14 16:25:08 -0800

    updated ml_project2

--- widefield/dynamics/ml_project2.py
+++ widefield/dynamics/ml_project2.py
@@ -52,11 +52,11 @@ region_data_test = {'Y': region_data[183000:-20000, :], 'X': regressor_data[1830
 # pickle.dump(lr,open(basepath + "ml_project/lr.pkl",'w'))
 lr = pickle.load(open(basepath + "ml_project/lr.pkl",'r'))
 
-# print "Fitting LGSSM"
+print "Fitting LGSSM"
 # # Fit EM parameters for the model, based on the sampled data
-# model = LinearGaussianSSM(A=np.copy(lr.coefficients.T), C=np.eye(21))
-# model.fit_em(region_data_train['Y'].T, max_iters=1000, exclude_list=['C'])
-# pickle.dump(model,open(basepath + "ml_project/lgssm.pkl",'w'))
+model = LinearGaussianSSM(A=np.copy(lr.coefficients.T), C=np.eye(21))
+model.fit_em(region_data_train['Y'].T, max_iters=1000, exclude_list=['C'])
+pickle.dump(model,open(basepath + "ml_project/lgssm.pkl",'w'))
 # model = pickle.load(open(basepath + "ml_project/lgssm.pkl",'r'))
 
 print "Doing linear regression - supervised case"
@@ -67,7 +67,7 @@ lr2 = pickle.load(open(basepath + "ml_project/lr_supervised.pkl",'r'))
 
 print "Fitting LGSSM - supervised case"
 # Fit EM parameters for the model, based on the sampled data
-model2 = LinearGaussianSSM(A=np.copy(lr2.coefficients[4:].T), B=np.copy(lr2.coefficients[0:4].T), C=np.eye(21))
-model2.fit_em(region_data_train['Y'].T, region_data_train['X'].T, max_iters=1000, exclude_list=['C'])
-pickle.dump(model2,open(basepath + "ml_project/lgssm_supervised.pkl",'w'))
-# model2 = pickle.load(open(basepath + "ml_project/lgssm_supervised.pkl",'r'))
+# model2 = LinearGaussianSSM(A=np.copy(lr2.coefficients[4:].T), B=np.copy(lr2.coefficients[0:4].T), C=np.eye(21))
+# model2.fit_em(region_data_train['Y'].T, region_data_train['X'].T, max_iters=1000, exclude_list=['C'])
+# pickle.dump(model2,open(basepath + "ml_project/lgssm_supervised.pkl",'w'))
+model2 = pickle.load(open(basepath + "ml_project/lgssm_supervised.pkl",'r'))
