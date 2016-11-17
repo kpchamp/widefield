f4591279792767c92623ddfc74f037b76204de29
kpchamp <kpchamp@uw.edu>
2016-11-16 20:36:16 -0800

    updated fa_model_selection

--- widefield/dimreduction/fa_model_selection.py
+++ widefield/dimreduction/fa_model_selection.py
@@ -12,7 +12,7 @@ tb_open.close()
 
 # Do model selection for Factor Analysis model
 ll = np.load(basepath + 'fa_loglikelihoods.npy').tolist()
-for n_components in np.arange(2010,5001,10):
+for n_components in np.arange(2010,3001,10):
     fa_model = FactorAnalysis(n_components=n_components+1)
     fa_model.fit(image_data_train)
     ll.append(fa_model.score(image_data_test))
