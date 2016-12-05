344b24c6aa29cf1995a930c01278dbc3202253ce
kpchamp <kpchamp@uw.edu>
2016-12-04 23:25:36 -0800

    updated ml_project2

--- widefield/dynamics/scripts/ml_project2.py
+++ widefield/dynamics/scripts/ml_project2.py
@@ -10,42 +10,48 @@ import time
 # Import
 basepath = "/suppscr/riekesheabrown/kpchamp/data/m187474/150804/"
 summary_data_path = basepath + "SummaryTimeSeries.pkl"
-summary_data = pd.read_pickle(summary_data_path)
 
-# replace NaNs with 0s
-summary_data['behavioral_measurables']['running_speed'][np.where(np.isnan(summary_data['behavioral_measurables']['running_speed']))[0]] = 0
+construct_dataset = False
+if construct_dataset:
+    summary_data = pd.read_pickle(summary_data_path)
 
-include_orientations = False
-if include_orientations:
-    # Get stimulus orientations into array
-    stimulus_contrasts = np.squeeze(summary_data['session_dataframe'].as_matrix(columns=['Contrast']))
-    stimulus_orientations = np.zeros(summary_data['stimulus'].shape)
-    stimulus_orientations[np.where(summary_data['stimulus'] != 0)[0]] = np.squeeze(summary_data['session_dataframe'].as_matrix(columns=['Ori']))[np.where(stimulus_contrasts != 0)[0]]
+    # replace NaNs with 0s
+    summary_data['behavioral_measurables']['running_speed'][np.where(np.isnan(summary_data['behavioral_measurables']['running_speed']))[0]] = 0
 
-    # Get regressors into format for regression
-    regressor_data = np.vstack((stimulus_contrasts, stimulus_orientations, summary_data['behavioral_measurables']['licking'],
-                                summary_data['behavioral_measurables']['rewards'], summary_data['behavioral_measurables']['running_speed'])).T
-    U_labels = ['stimulus contrast', 'stimulus orientation', 'licking', 'rewards', 'running speed']
+    include_orientations = False
+    if include_orientations:
+        # Get stimulus orientations into array
+        stimulus_contrasts = np.squeeze(summary_data['session_dataframe'].as_matrix(columns=['Contrast']))
+        stimulus_orientations = np.zeros(summary_data['stimulus'].shape)
+        stimulus_orientations[np.where(summary_data['stimulus'] != 0)[0]] = np.squeeze(summary_data['session_dataframe'].as_matrix(columns=['Ori']))[np.where(stimulus_contrasts != 0)[0]]
+
+        # Get regressors into format for regression
+        regressor_data = np.vstack((stimulus_contrasts, stimulus_orientations, summary_data['behavioral_measurables']['licking'],
+                                    summary_data['behavioral_measurables']['rewards'], summary_data['behavioral_measurables']['running_speed'])).T
+        U_labels = ['stimulus contrast', 'stimulus orientation', 'licking', 'rewards', 'running speed']
+    else:
+        # Get regressors into format for regression
+        regressor_data = np.vstack((summary_data['stimulus'], summary_data['behavioral_measurables']['licking'],
+                                    summary_data['behavioral_measurables']['rewards'], summary_data['behavioral_measurables']['running_speed'])).T
+        U_labels = ['stimulus contrast', 'licking', 'rewards', 'running speed']
+
+
+    n_time_steps = summary_data['ROIs_F'][summary_data['ROIs_F'].keys()[0]].size
+    n_regions = len(summary_data['ROIs_F'].keys())
+    region_data = np.zeros((n_time_steps, n_regions))
+    region_labels = []
+    for i,key in enumerate(sorted(summary_data['ROIs_F'].keys())):
+        region_data[:, i] = (summary_data['ROIs_F'][key] - summary_data['ROIs_F0'][key]) / summary_data['ROIs_F0'][key]
+        region_labels.append(key)
+
+    # Construct training and test sets
+    train = {'Y': region_data[20000:70000, :], 'U': regressor_data[20000:70000, :], 'U_labels': U_labels, 'Y_labels': region_labels}
+    test = {'Y': region_data[70000:-20000, :], 'U': regressor_data[70000:-20000, :], 'U_labels': U_labels, 'Y_labels': region_labels}
+    pickle.dump(train, open(basepath + "ml_project/train.pkl",'w'))
+    pickle.dump(test, open(basepath + "ml_project/test.pkl",'w'))
 else:
-    # Get regressors into format for regression
-    regressor_data = np.vstack((summary_data['stimulus'], summary_data['behavioral_measurables']['licking'],
-                                summary_data['behavioral_measurables']['rewards'], summary_data['behavioral_measurables']['running_speed'])).T
-    U_labels = ['stimulus contrast', 'licking', 'rewards', 'running speed']
-
-
-n_time_steps = summary_data['ROIs_F'][summary_data['ROIs_F'].keys()[0]].size
-n_regions = len(summary_data['ROIs_F'].keys())
-region_data = np.zeros((n_time_steps, n_regions))
-region_labels = []
-for i,key in enumerate(sorted(summary_data['ROIs_F'].keys())):
-    region_data[:, i] = (summary_data['ROIs_F'][key] - summary_data['ROIs_F0'][key]) / summary_data['ROIs_F0'][key]
-    region_labels.append(key)
-
-# Construct training and test sets
-train = {'Y': region_data[20000:70000, :], 'U': regressor_data[20000:70000, :], 'U_labels': U_labels, 'Y_labels': region_labels}
-test = {'Y': region_data[70000:-20000, :], 'U': regressor_data[70000:-20000, :], 'U_labels': U_labels, 'Y_labels': region_labels}
-pickle.dump(train, open(basepath + "ml_project/train.pkl",'w'))
-pickle.dump(test, open(basepath + "ml_project/test.pkl",'w'))
+    train = pickle.load(open(basepath + "ml_project/train.pkl",'r'))
+    test = pickle.load(open(basepath + "ml_project/test.pkl",'r'))
 
 fit_original_model = False
 fit_original_LR = False
