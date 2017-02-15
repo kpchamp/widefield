b78d8e11c163f8a99ead4334eb4869e6910f817e
kpchamp <kpchamp@uw.edu>
2017-02-15 15:37:58 -0800

    switched movie dimensions in alignment code

--- widefield/tools/alignment.py
+++ widefield/tools/alignment.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def align_by_stimulus(movie, stimulus_vector, time_before, time_after):
-    n_pixels, n_frames = movie.shape
+    n_frames, n_pixels = movie.shape
     stimulus_times = np.where(stimulus_vector != 0)[0]
     n_trials = stimulus_times.size
     trial_length = time_after + time_before
@@ -14,7 +14,7 @@ def align_by_stimulus(movie, stimulus_vector, time_before, time_after):
     end_times = np.minimum(n_frames, stimulus_times + time_after)
     new_movie = np.empty((n_trials, trial_length, n_pixels))
     for i in range(n_trials):
-        new_movie[i] = movie[:,start_times[i]:end_times[i]].T
+        new_movie[i] = movie[start_times[i]:end_times[i],:]
     return new_movie
 
 # Takes a movie that is organized by trials and returns a movie organized sequentially
@@ -25,11 +25,11 @@ def reshape_trial_to_sequence(movie, left_truncation=0, right_truncation=0):
     n_samples = n_trials*(n_timesteps - left_truncation - right_truncation)
     start_idx = left_truncation
     if right_truncation == 0:
-        return movie[:,left_truncation:,:].reshape((n_samples, n_pixels)).T
+        return movie[:,left_truncation:,:].reshape((n_samples, n_pixels))
     else:
-        return movie[:,left_truncation:-right_truncation,:].reshape((n_samples, n_pixels)).T
+        return movie[:,left_truncation:-right_truncation,:].reshape((n_samples, n_pixels))
 
 
 def reshape_sequence_to_trial(movie, n_trials):
     n_pixels, n_samples = movie.shape
-    return movie.T.reshape((n_trials, n_samples/n_trials, n_pixels))
+    return movie.reshape((n_trials, n_samples/n_trials, n_pixels))
