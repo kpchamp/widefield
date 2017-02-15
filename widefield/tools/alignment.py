d0573d697692c82ccea08658a6207203f9315a97
kpchamp <kpchamp@uw.edu>
2017-02-15 14:41:18 -0800

    fixed bug in stim aligning code

--- widefield/tools/alignment.py
+++ widefield/tools/alignment.py
@@ -9,7 +9,7 @@ def align_by_stimulus(movie, stimulus_vector, time_before, time_after):
     n_pixels, n_frames = movie.shape
     stimulus_times = np.where(stimulus_vector != 0)[0]
     n_trials = stimulus_times.size
-    trial_length = time_after - time_before
+    trial_length = time_after + time_before
     start_times = np.maximum(0, stimulus_times - time_before)
     end_times = np.minimum(n_frames, stimulus_times + time_after)
     new_movie = np.empty((n_trials, trial_length, n_pixels))
