c6362a91b4b7e55bb16104e72376155cb878bfec
kpchamp <kpchamp@uw.edu>
2017-02-15 15:03:58 -0800

    added methods for reshaping stimulus-aligned data

--- widefield/tools/alignment.py
+++ widefield/tools/alignment.py
@@ -16,3 +16,20 @@ def align_by_stimulus(movie, stimulus_vector, time_before, time_after):
     for i in range(n_trials):
         new_movie[i] = movie[:,start_times[i]:end_times[i]].T
     return new_movie
+
+# Takes a movie that is organized by trials and returns a movie organized sequentially
+# (3-dim to 2-dim). Left and right truncation values can be used if you don't want to
+# use the full length of the trial.
+def reshape_trial_to_sequence(movie, left_truncation=0, right_truncation=0):
+    n_trials, n_timesteps, n_pixels = movie.shape
+    n_samples = n_trials*(n_timesteps - left_truncation - right_truncation)
+    start_idx = left_truncation
+    if right_truncation == 0:
+        return movie[:,left_truncation:,:].reshape((n_samples, n_pixels)).T
+    else:
+        return movie[:left_truncation:-right_truncation,:].reshape((n_samples, n_pixels)).T
+
+
+def reshape_sequence_to_trial(movie, n_trials):
+    n_pixels, n_samples = movie.shape
+    return movie.T.reshape((n_trials, n_samples/n_trials, n_pixels))
