9da637bb76852a8ad459380b24001c726252bf5b
kpchamp <kpchamp@uw.edu>
2017-03-13 11:57:47 -0700

    added method for turning an array into a movie and saving as an mp4

---
+++ widefield/tools/movie_tools.py
@@ -0,0 +1,21 @@
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+
+def make_movie(filename, mov, caxis):
+    FFMpegWriter = manimation.writers['ffmpeg']
+    metadata = dict(title='Movie Test', artist='Matplotlib',
+                    comment='Movie support!')
+    writer = FFMpegWriter(fps=15, metadata=metadata)
+
+    fig = plt.figure()
+    plt.imshow(mov[0], interpolation='nearest')
+    plt.clim(caxis)
+
+    with writer.saving(fig, filename, 100):
+        for i in range(1,mov.shape[0]):
+            plt.imshow(mov[i], interpolation='nearest')
+            plt.clim(caxis)
+            writer.grab_frame()
