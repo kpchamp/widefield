9a5a2be16a880ab33e2e69993be3f06ff405907e
kpchamp <kpchamp@uw.edu>
2017-03-16 11:40:18 -0700

    updated make_movie function to take dpi as an argument

--- widefield/tools/movie_tools.py
+++ widefield/tools/movie_tools.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
 
 
-def make_movie(filename, mov, caxis):
+def make_movie(filename, mov, caxis, dpi=100):
     FFMpegWriter = manimation.writers['ffmpeg']
     metadata = dict(title='Movie Test', artist='Matplotlib',
                     comment='Movie support!')
@@ -14,7 +14,7 @@ def make_movie(filename, mov, caxis):
     plt.imshow(mov[0], interpolation='nearest')
     plt.clim(caxis)
 
-    with writer.saving(fig, filename, 100):
+    with writer.saving(fig, filename, dpi):
         for i in range(1,mov.shape[0]):
             plt.imshow(mov[i], interpolation='nearest')
             plt.clim(caxis)
