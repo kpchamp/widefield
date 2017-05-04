e2e05e3325b8d3428310168ce3f93a00b4b4ac67
kpchamp <kpchamp@uw.edu>
2017-05-04 14:32:06 -0700

    more printing debugging output

--- widefield/regression/linregress.py
+++ widefield/regression/linregress.py
@@ -154,10 +154,10 @@ class DynamicRegression:
                         self.coefficients[input_idxs,i] = la.lstsq(input_matrix[:,input_idxs], output_matrix[:,i])[0]
                     else:
                         input_idxs = np.arange(n_outputs)
-                        print input_inclusions[i][0]
                         for j in input_inclusions[i]:
                             input_idxs = np.concatenate((input_idxs,n_outputs+np.arange(j*self.convolution_length,(j+1)*self.convolution_length)))
                         input_idxs = np.concatenate((input_idxs,np.array([n_inputs-1])))
+                        print input_idxs
                         self.coefficients[input_idxs,i] = la.lstsq(input_matrix[:,input_idxs], output_matrix[:,i])[0]
         if self.fit_offset:
             self.offset = self.coefficients[0]
